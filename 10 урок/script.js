995669038c6e888467a150acaae02a9db8b6fca8
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-02-10 21:18:16 +0300

    Все

--- 10 урок/script.js
+++ 10 урок/script.js
@@ -1,19 +1,9 @@
-//// Восстановить порядок книг.
-//// Заменить картинку заднего фона на другую из папки image
-//// Исправить заголовок в книге 3 (Получится - "Книга 3. this и Прототипы Объектов")
-// //Удалить рекламу со страницы
-// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
-
 let books = document.querySelectorAll('.books'),
   book = document.querySelectorAll('.book');
-console.log(book, books);
 
 books[0].insertBefore(book[1], book[0]);
 books[0].insertBefore(book[4], book[3]);
 books[0].insertBefore(book[2], null);
-
-let bookTwo = document.querySelectorAll('book')[2];
-console.log(bookTwo);
 
 let header = document.getElementsByTagName('h2')[2];
 header.innerHTML = 'Книга 3. this и Прототипы Объектов';
@@ -23,11 +13,23 @@ document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 let adv = document.querySelector('.adv');
 adv.style.display = 'none';
 
+let bookSix = document.querySelectorAll('ul')[5];
 let newLi = document.createElement('li');
 newLi.textContent = 'Глава 8: За пределами ES6';
-book[2].appendChild(newLi);
+bookSix.appendChild(newLi);
+let liSix = bookSix.querySelectorAll('li');
+bookSix.insertBefore(newLi, liSix[9]);
 
-// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+let bookTwo = document.querySelectorAll('ul')[1];
+let liTwo = bookTwo.querySelectorAll('li');
+bookTwo.insertBefore(liTwo[2], liTwo[8]);
+bookTwo.insertBefore(liTwo[6], liTwo[4]);
+bookTwo.insertBefore(liTwo[8], liTwo[4]);
+bookTwo.insertBefore(liTwo[9], liTwo[2]);
 
-let li = book[0].querySelectorAll('li');
-book[0].insertBefore(li[1], li[3]);
+let bookFive = document.querySelectorAll('ul')[4];
+let liFive = bookFive.querySelectorAll('li');
+bookFive.insertBefore(liFive[9], liFive[2]);
+bookFive.insertBefore(liFive[5], liFive[8]);
+bookFive.insertBefore(liFive[2], liFive[4]);
+bookFive.insertBefore(liFive[4], liFive[2]);
