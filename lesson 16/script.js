654cf4d1c0c03d4c56c8775165dcb16953fcf6d2
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-02 19:31:28 +0300

    Update script.js

--- lesson 16/script.js
+++ lesson 16/script.js
@@ -15,12 +15,3 @@ let first = new First('first');
 let second = new Second('second');
 second.hello();
 
-
-// second.hello1();
-
-
-// class Second {
-//   hello() {
-//     console.log('А я наследуемый метод!');
-//   }
-// }
